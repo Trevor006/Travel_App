94e6ee22eed44ac374ac1b6377707dbdf4f4991f
valerie-commits <owuorvalerie@gmail.com>
2025-04-09 12:34:00 +0300

    Format made

--- lib/widgets/search.dart
+++ lib/widgets/search.dart
@@ -1,8 +1,8 @@
 import "package:flutter/material.dart";
 
-
 /// Flutter code sample for [SearchBar].
-class CustomSearchBar extends StatefulWidget { // Renamed class
+class CustomSearchBar extends StatefulWidget {
+  // Renamed class
   const CustomSearchBar({super.key});
 
   @override
@@ -13,13 +13,14 @@ class _CustomSearchBarState extends State<CustomSearchBar> {
   bool isDark = false;
   final SearchController controller = SearchController();
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: SearchAnchor(
         builder: (BuildContext context, SearchController controller) {
-          return SearchBar( // This now correctly refers to Flutter's SearchBar
+          return SearchBar(
+            // This now correctly refers to Flutter's SearchBar
             controller: controller,
             padding: const WidgetStatePropertyAll<EdgeInsets>(
               EdgeInsets.symmetric(horizontal: 16.0),
@@ -47,7 +48,10 @@ class _CustomSearchBarState extends State<CustomSearchBar> {
             ],
           );
         },
-        suggestionsBuilder: (BuildContext context, SearchController controller) {
+        suggestionsBuilder: (
+          BuildContext context,
+          SearchController controller,
+        ) {
           return List<ListTile>.generate(5, (int index) {
             final String item = 'item $index';
             return ListTile(
