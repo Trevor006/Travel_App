5f6ca6a0badd85bc7af3eebf2927add7d442e58f
Trevor Mugo <mugotervor71@gmail.com>
2025-04-10 11:23:27 +0300

    Designing pages

--- lib/pages/destination.dart
+++ lib/pages/destination.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/widgets/search.dart';
 
 class DestinationPage extends StatefulWidget {
   const DestinationPage({super.key, required this.title});
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/widgets/image.dart';
+import 'package:travel_app/widgets/search.dart';
+import 'package:travel_app/widgets/text.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -15,10 +18,46 @@ class _MyHomePageState extends State<MyHomePage> {
     return Container(
       padding: EdgeInsets.all(10.0),
       color: Colors.white,
-      alignment: Alignment.topLeft,
-      child: Column(children: <Widget>[
+      child: Column(
+        children: <Widget>[
+          Expanded(flex: 2, child: CustomSearchBar()),
 
-             ],),
+          Expanded(
+            flex: 5,
+            child: ImageWidget(
+              imagePath: "../assets/images/image.png",
+              imageVariant: "large",
+            ),
+          ),
+
+          Expanded(
+            flex: 5,
+            child: Column(
+              
+              children: <Widget>[
+                TextWidget(
+                  displayText: "Lempuyang Temple",
+                  styleVariant: "bold",
+                ),
+                TextWidget(
+                  displayText: "South C, Kenya . 200km",
+                  styleVariant: "notBold",
+                ),
+                Row(
+                  children: <Widget>[
+                    TextWidget(displayText: "KSH 250", styleVariant: "bold"),
+                    TextWidget(
+                      displayText: " per night",
+                      styleVariant: "light",
+                    ),
+                  ],
+                ),
+      
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
