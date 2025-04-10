d7f106881b66d3102728a52a624f6665e02549df
Grace Kyalo <kyalograce21@gmail.com>
2025-04-10 13:32:33 +0300

    Good Progress

--- lib/pages/destination.dart
+++ lib/pages/destination.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/pages/homepage.dart';
 import 'package:travel_app/widgets/button.dart';
-import 'package:travel_app/widgets/icons.dart';
 import 'package:travel_app/widgets/image.dart';
-import 'package:travel_app/widgets/search.dart';
-import 'package:travel_app/widgets/svg.dart';
 import 'package:travel_app/widgets/text.dart';
 
 class DestinationPage extends StatefulWidget {
@@ -20,7 +17,7 @@ class _DestinationPageState extends State<DestinationPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(10.0),
+      padding: EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 20.0),
       color: Colors.white,
       alignment: Alignment.topLeft,
       child: Column(
@@ -38,26 +35,36 @@ class _DestinationPageState extends State<DestinationPage> {
             child: Icon(Icons.arrow_back),
           ),
 
+          SizedBox(height: 20,),
+
           Expanded(
-            flex: 9,
+            flex: 6,
             child: ImageWidget(
-              imagePath: "../assets/images/image.png",
+              imagePath: "./assets/images/image.png",
               imageVariant: "large",
             ),
           ),
 
+          SizedBox(height: 20,),
+
           Expanded(
-            flex: 5,
+            flex: 6,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              textDirection: TextDirection.ltr,
               children: <Widget>[
                 TextWidget(
                   displayText: "Lempuyang Temple",
                   styleVariant: "bold",
                 ),
+                SizedBox(height: 10,),
+
                 TextWidget(
                   displayText: "South C, Kenya . 200km",
                   styleVariant: "notBold",
                 ),
+                SizedBox(height: 10,),
+
                 Row(
                   children: <Widget>[
                     TextWidget(displayText: "KSH 250", styleVariant: "bold"),
@@ -65,8 +72,26 @@ class _DestinationPageState extends State<DestinationPage> {
                       displayText: " per night",
                       styleVariant: "light",
                     ),
+                    
                   ],
                 ),
+                SizedBox(height: 10,),
+
+                Divider(color: Colors.grey),
+                    TextWidget(
+                      displayText: "Description",
+                      styleVariant: "bold",
+                    ),
+
+                    SizedBox(height: 10,),
+
+                    TextWidget(
+                      displayText:
+                          "Lempuyang Temple is a Hindu temple located on the slopes of Mount Lempuyang in Bali, Indonesia. It is one of the oldest and most highly regarded temples on the island and is known for its series of ornate gates, each guarded by menacing demons and gods",
+                      styleVariant: "light",
+                    ),
+
+                    SizedBox(height: 20,),
                 ButtonWidget(title: "Book Now", onPressed: () {}),
               ],
             ),
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/pages/destination.dart';
-import 'package:travel_app/widgets/icons.dart';
 import 'package:travel_app/widgets/image.dart';
 import 'package:travel_app/widgets/search.dart';
 import 'package:travel_app/widgets/svg.dart';
 import 'package:travel_app/widgets/text.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -20,9 +18,11 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(10.0),
+      padding: EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 20.0),
       color: Colors.white,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        textDirection: TextDirection.ltr,
         children: <Widget>[
           Expanded(flex: 2, child: CustomSearchBar()),
 
@@ -31,34 +31,40 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                SvgButtonWidget(assetPath: "../assets/icons/vector.svg"),
-                SvgButtonWidget(assetPath: "../assets/icons/beach.svg"),
-                SvgButtonWidget(assetPath: "../assets/icons/swimming.svg"),
-                SvgButtonWidget(assetPath: "../assets/icons/island.svg"),
-                SvgButtonWidget(assetPath: "../assets/icons/building.svg"),
+                SvgButtonWidget(assetPath: "./assets/icons/Vector.svg"),
+                SvgButtonWidget(assetPath: "./assets/icons/beach.svg"),
+                SvgButtonWidget(assetPath: "./assets/icons/swimming.svg"),
+                SvgButtonWidget(assetPath: "./assets/icons/island.svg"),
+                SvgButtonWidget(assetPath: "./assets/icons/building.svg"),
               ],
             ),
           ),
 
           Expanded(
             flex: 9,
-            child: ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> DestinationPage(title: "Destinations")),);
-            }, 
-            child: ImageWidget(
-              imagePath: "../assets/images/image.png",
-              imageVariant: "large",
-            ),)
-            
-            
-            
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder:
+                        (context) => DestinationPage(title: "Destinations"),
+                  ),
+                );
+              },
+              child: ImageWidget(
+                imagePath: "./assets/images/image.png",
+                imageVariant: "large",
+              ),
+            ),
           ),
 
           Expanded(
             flex: 5,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              textDirection: TextDirection.ltr,
               children: <Widget>[
-                
                 TextWidget(
                   displayText: "Lempuyang Temple",
                   styleVariant: "bold",
@@ -74,6 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       displayText: " per night",
                       styleVariant: "light",
                     ),
+                    
                   ],
                 ),
               ],
